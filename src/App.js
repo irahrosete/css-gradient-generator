bb6032a201846ad6bbe5c59a76c19d8fa22be0b8
irah rosete <irah.rosete@outlook.com>
2021-05-21 11:44:40 +1000

    add colorpicker and function to handle change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Component } from 'react'
+import styled from 'styled-components'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledMain = styled.main`
+  background-color: ${props => props.backgroundColor};
+  font-family: 'Courier New', Courier, monospace;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%
+`
+
+class App extends Component {
+  state = {
+    backgroundColor: "#234fde"
+  }
+
+  handleColorChange = (event) => {
+    const newColor = event.target.value
+    // console.log(newColor)
+    this.setState({ backgroundColor: newColor})
+  }
+
+	render() {
+    const { backgroundColor } = this.state
+		return (
+			<StyledMain backgroundColor={backgroundColor}>
+				<h1>CSS Gradient Generator</h1>
+				<input type="color" value={backgroundColor} onChange={this.handleColorChange} />
+			</StyledMain>
+		)
+	}
 }
 
 export default App;
