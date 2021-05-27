e735702caf9a73fc1c068a25af7b8ff3de62a6f0
irah rosete <irah.rosete@outlook.com>
2021-05-27 13:50:19 +1000

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CSS Gradient Generator
-Generates the CSS for a background gradient using the colours you choose.
+Generates background gradient CSS using the colours you choose.
--- src/App.js
+++ src/App.js
@@ -110,36 +110,3 @@ const App = () => {
 }
 
 export default App;
-
-// -----
-// class OldApp extends Component {
-//   state = {
-//     backgroundColor: ["#c0c0c0", "#c0c0c0", "#c0c0c0"]
-//   }
-
-//   handleColorChange = (event, i) => {
-//     const newColor = event.target.value
-//     // create a copy of the backgroundColor array
-//     const newBackgroundColor = [...this.state.backgroundColor]
-//     // change the value of the array to the new color
-//     newBackgroundColor[i] = newColor
-//     console.log(newColor)
-//     this.setState({ backgroundColor: newBackgroundColor})
-//   }
-
-// 	render() {
-//     const { backgroundColor } = this.state
-// 		return (
-// 			<StyledMain backgroundColor={backgroundColor}>
-// 				<h1>CSS Gradient Generator</h1>
-//         {backgroundColor.map((color, i) => {
-//           return (
-//             <input type="color"
-//               value={color}
-//               onChange={(event) => this.handleColorChange(event, i)} />
-//           )
-//         })}
-// 			</StyledMain>
-// 		)
-// 	}
-// }
